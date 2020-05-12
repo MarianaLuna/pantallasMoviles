05265400427a41374267b96d9a1f1960a591218a
MarianaLuna <55517977+MarianaLuna@users.noreply.github.com>
2020-05-11 21:02:11 -0500

    Add files via upload

---
+++ Pantallas_Proyecto/app/src/main/java/com/example/pantallas_proyecto/Activity_QuizzIntroduccion.java
@@ -0,0 +1,14 @@
+package com.example.pantallas_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_QuizzIntroduccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__quizz_introduccion);
+    }
+}
---
+++ Pantallas_Proyecto/app/src/main/java/com/example/pantallas_proyecto/Activity_Subcategoria4.java
@@ -0,0 +1,14 @@
+package com.example.pantallas_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Subcategoria4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__subcategoria4);
+    }
+}
